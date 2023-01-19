3e89a3b7806614dd3625a4edd1cd86be2caae42b
Kadievka Salcedo <kadievkaasv@ext.zara.com>
2023-01-19 10:13:30 +0100

    add more

--- 5 Decorator/ts/decorator.ts
+++ 5 Decorator/ts/decorator.ts
@@ -1,5 +1,6 @@
 interface ComponentInterface {
   getDetail(): string;
+
 }
 
 class DetailComponent implements ComponentInterface {
@@ -17,14 +18,14 @@ class DetailComponent implements ComponentInterface {
 
 abstract class ProductDecorator implements ComponentInterface {
 
-  protected component: ComponentInterface;
+  protected component?: ComponentInterface;
 
-  constructor(component: ComponentInterface) {
+  constructor(component?: ComponentInterface) {
     this.component = component;
   }
 
-  getDetail(): string{
-    return this.component.getDetail();
+  getDetail(): string {
+    return this.component ? this.component.getDetail() : "";
   };
 
 }
@@ -35,14 +36,14 @@ class CommercialProductDecorator extends ProductDecorator {
   private tradename: string;
   private brand: string;
 
-  constructor(component: ComponentInterface, tradename: string, brand: string) {
+  constructor(tradename: string, brand: string, component?: ComponentInterface) {
     super(component);
     this.tradename = tradename;
     this.brand = brand;
   }
 
   getDetail(): string{
-    return `${this.tradename} ${this.brand} ${this.component.getDetail()}`;
+    return `${this.tradename} ${this.brand} ${this.component ? this.component.getDetail() : ""}`;
   }
 }
 
@@ -51,14 +52,32 @@ class StoreProductDecorator extends ProductDecorator {
 
   private price: number;
 
-  constructor(component: ComponentInterface, price: number) {
+  constructor(price: number, component?: ComponentInterface) {
     super(component);
     this.price = price;
   }
 
   getDetail(): string{
-    return `${this.price}$ ${this.component.getDetail()}`;
+    return `${this.price}$ ${this.component ? this.component.getDetail() : ""}`;
   }
+}
+
+class NameComponent extends ProductDecorator {
+
+  protected name: string;
+  protected component?: ComponentInterface;
+
+  constructor(name: string, component?: ComponentInterface) {
+
+    super(component);
+
+    this.name = name;
+    this.component = component;
+  }
+
+  getDetail(): string {
+    return `${this.name}$ ${this.component?.getDetail()}`
+  };
 }
 
 // execute 
@@ -66,20 +85,26 @@ class StoreProductDecorator extends ProductDecorator {
 const productComponent = new DetailComponent("Cerveza");
 console.log(productComponent.getDetail());
 
-const commercialProductDecorator = new CommercialProductDecorator(productComponent, "London Porter", "Fuller's");
+const commercialProductDecorator = new CommercialProductDecorator("London Porter", "Fuller's", productComponent);
 console.log(commercialProductDecorator.getDetail());
 
-const storeProductDecorator = new StoreProductDecorator(commercialProductDecorator, 25);
+const storeProductDecorator = new StoreProductDecorator(25, commercialProductDecorator);
 console.log(storeProductDecorator.getDetail());
 
-const storeProductDecorator2 = new StoreProductDecorator(productComponent, 13);
+const storeProductDecorator2 = new StoreProductDecorator(13, productComponent);
 console.log(storeProductDecorator.getDetail());
 
-const commercialProductDecorator2 = new CommercialProductDecorator(storeProductDecorator, "New Others", "Names");
+const commercialProductDecorator2 = new CommercialProductDecorator("New Others", "Names", storeProductDecorator);
 console.log(commercialProductDecorator2.getDetail());
 
-const commercialProductDecorator3 = new CommercialProductDecorator(storeProductDecorator2, "New Others", "Names");
+const commercialProductDecorator3 = new CommercialProductDecorator("New Others", "Names", storeProductDecorator2);
 console.log(commercialProductDecorator3.getDetail());
 
-const storeProductDecorator3 = new StoreProductDecorator(productComponent, 5);
+const storeProductDecorator3 = new StoreProductDecorator(5, productComponent);
 console.log(storeProductDecorator3.getDetail());
+
+const nameComponent = new NameComponent("Cerveza", new StoreProductDecorator(15));
+console.log(nameComponent.getDetail());
+
+const nameComponent2 = new NameComponent("Cerveza", new CommercialProductDecorator("London Porter", "Fuller's", new StoreProductDecorator(15)));
+console.log(nameComponent2.getDetail());
